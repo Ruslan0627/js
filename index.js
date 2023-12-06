f7a214386275bf10fbc616c1b03b521445cbb69e
Ruslan0627 <mamedovruslan627@gmail.com>
2023-12-06 16:08:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//Задание №1
+const myName = "Руслан";
+console.log(myName);
+
+const myAge = 17;
+console.log(myAge);
+
+const myColor = "Black";
+console.log(myColor);
+
+//Задание №2
+const numberFirst = 1;
+console.log(numberFirst);
+
+const numberSecond = 2;
+console.log(numberSecond);
+
+console.log(numberFirst + numberSecond);
+
+//Задание №3
+const mySurname = "Мамедов";
+console.log(mySurname);
+
+const myPatronymic = "Видадиевич";
+console.log(myPatronymic);
+
+console.log(mySurname + " " + myName + " " + myPatronymic);
+
+//Задание №4
+let numberThird = 3;
+numberThird +=5;
+console.log(numberThird);
+
+//Задание №5
+console.log(numberFirst < numberSecond);
+
+
