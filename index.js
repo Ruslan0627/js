d6fc82209c3ac9065ba2417a67f5efd67ef2f10a
Ruslan0627 <mamedovruslan627@gmail.com>
2023-12-13 10:16:59 +0300

    Сделал первую половину ДЗ второго урока

--- index.js
+++ index.js
@@ -1,37 +1,105 @@
-//Задание №1
-const myName = "Руслан";
-console.log(myName);
+// Дз№2
 
-const myAge = 17;
-console.log(myAge);
+// Задание №1 Switch case
 
-const myColor = "Black";
-console.log(myColor);
+const dayOfWeek = 1
 
-//Задание №2
-const numberFirst = 1;
-console.log(numberFirst);
+switch (dayOfWeek) {
+    case 1 : console.log("Понедельник");
+    break;
+    case 2 : console.log("Вторник");
+    break;
+    case 3 : console.log("Среда");
+    break;
+    case 4 : console.log("Четверг");
+    break;
+    case 5 : console.log("Пятница");
+    break;   
+    case 6 : console.log("Суббота");
+    break;  
+    case 7 : console.log("Воскресенье");
+    break;
+    default:console.log("Такого дня недели нет");
+}
 
-const numberSecond = 2;
-console.log(numberSecond);
+const month = 1
 
-console.log(numberFirst + numberSecond);
+switch (month) {
+case 1 : console.log("Зима");
+break;
+case 2 : console.log("Зима");
+break;
+case 3 : console.log("Весна");
+break;
+case 4 : console.log("Весна");
+break;
+case 5 : console.log("Весна");
+break;   
+case 6 : console.log("Лето");
+break; 
 
-//Задание №3
-const mySurname = "Мамедов";
-console.log(mySurname);
+case 7 : console.log("Лето");
+break;
 
-const myPatronymic = "Видадиевич";
-console.log(myPatronymic);
+case 8 : console.log("Лето");
+break;
 
-console.log(mySurname + " " + myName + " " + myPatronymic);
+case 9 : console.log("Осень");
+break;
 
-//Задание №4
-let numberThird = 3;
-numberThird +=5;
-console.log(numberThird);
+case 10 : console.log("Осень");
+break; 
 
-//Задание №5
-console.log(numberFirst < numberSecond);
+case 11 : console.log("Осень");
+break;
+
+case 12 : console.log("Зима");
+break;
+
+default:console.log("Такого месяца нет");
+}
+
+// Задание №2 Тернарный оператор
+
+const isRaning = "Идёт дождь"
+
+const dry = "Cухо"
+
+const decision = isRaning === isRaning ? "Идёт дождь" :"Cухо"
+console.log(decision)
+
+const score = 40
+
+const gradeA = score >= 80 ? "Оценка A" : "" 
+console.log(gradeA);
+
+const gradeB = 80 > score && score >= 60 ? "Оценка B" : ""
+console.log(gradeB);
+
+const gradeC = 60 > score &&  score >= 40 ? "Оценка C" : ""
+console.log(gradeC);
+
+const gradeD = 40 > score && score >= 20 ? "Оценка D" : ""
+console.log(gradeD);
+
+const gradeF = 20 > score && score >= 0 ? "Оценка F " : ""
+console.log(gradeF);
+
+// Задание №3 Булевая логика
+
+const hasMoney = true
+const isHungry = true
+
+console.log(`Пойду ли я в ресторан: ${hasMoney && isHungry ?"Да" : "Нет"} `);
+
+const isWeekend = true
+const hasPartyInvation = true
+
+console.log(`Пойду ли я на вечеринку: ${isWeekend || hasPartyInvation ?"Да" : "Нет"} `);
+
+
+
+
+
 
 
